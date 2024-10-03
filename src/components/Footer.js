ac9ec39169023adda68f306878cad9dc0e492135
Eric <eric.smith.9481@gmail.com>
2024-10-02 19:47:19 -0600

    built footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import { List, ListItem, Link, Image, Box, Flex } from "@chakra-ui/react";
 import React from "react";
 import Logo from "../assets/logo.svg";
+import logoVar from "../assets/logoVar.jpg"
 import Container from "./Container";
 import FlexContainer from "./FlexContainer";
 
 const Footer = () => {
   return (
-    <Box maxWidth="100%" bg="green.800">
+    <Box maxWidth="100%" bg="brand.100">
       <Flex
         as="footer"
         maxWidth="860px"
@@ -15,39 +16,40 @@ const Footer = () => {
         alignItems='center'
         justifyContent="space-between"
       >
-        <Image src={Logo} />
-        <Flex gap={20}>
-          <List>
-            <ListItem>
-              <Link>Home</Link>
-            </ListItem>
-            <ListItem>
-              <Link>About</Link>
-            </ListItem>
-            <ListItem>
-              <Link>Menu</Link>
-            </ListItem>
-            <ListItem>
-              <Link>Reservations</Link>
-            </ListItem>
-            <ListItem>
-              <Link>Order Online</Link>
-            </ListItem>
-            <ListItem>
-              <Link>Login</Link>
-            </ListItem>
-          </List>
-          <List>
-            <ListItem>Address</ListItem>
-            <ListItem>Phone Number</ListItem>
-            <ListItem>Email</ListItem>
-          </List>
-          <List>
-            <ListItem>Address</ListItem>
-            <ListItem>Phone Number</ListItem>
-            <ListItem>Email</ListItem>
-          </List>
-        </Flex>
+        <Image src={logoVar} />
+        
+          <Flex gap={32} marginTop={5} color='brand.300'>
+            <List>
+              <ListItem>
+                <Link>Home</Link>
+              </ListItem>
+              <ListItem>
+                <Link>About</Link>
+              </ListItem>
+              <ListItem>
+                <Link>Menu</Link>
+              </ListItem>
+              <ListItem>
+                <Link>Reservations</Link>
+              </ListItem>
+              <ListItem>
+                <Link>Order Online</Link>
+              </ListItem>
+              <ListItem>
+                <Link>Login</Link>
+              </ListItem>
+            </List>
+            <List>
+              <ListItem>Address</ListItem>
+              <ListItem>Phone Number</ListItem>
+              <ListItem>Email</ListItem>
+            </List>
+            <List>
+              <ListItem>Address</ListItem>
+              <ListItem>Phone Number</ListItem>
+              <ListItem>Email</ListItem>
+            </List>
+          </Flex>
       </Flex>
     </Box>
   );
