40a79593dceb07b5fc8b4f45569e37dfefab9d4f
Eric <eric.smith.9481@gmail.com>
2024-10-13 17:46:04 -0600

    fetchAPI

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,21 +14,49 @@ import OrderOnline from './OrderOnline'
 import Login from './Login'
 import BookingForm from './BookingForm'
 
+const seededRandom = function (seed) {
+  var m = 2**35 - 31;
+  var a = 185852;
+  var s = seed % m;
+  return function () {
+      return (s = s * a % m) / m;
+  };
+}
+
+const fetchAPI = function(date) {
+  let result = [];
+  let random = seededRandom(date.getDate());
+
+  for(let i = 17; i <= 23; i++) {
+      if(random() < 0.5) {
+          result.push(i + ':00');
+      }
+      if(random() < 0.5) {
+          result.push(i + ':30');
+      }
+  }
+  return result;
+};
+const submitAPI = function(formData) {
+  return true;
+};
+
 
 export const updateTimes = (state, action) => {
-  return state;
-}
+  switch (action.type) {
+    case "UPDATE_TIMES":
+      return fetchAPI(new Date(action.date)); 
+    default:
+      return state;
+  }
+};
+
 
 export const initializeTimes = () => {
-  return [
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-    "21:00",
-    "22:00",
-  ]      
-}
+  const today = new Date();
+  return fetchAPI(today); 
+};
+
 
 const Main = () => {
   
