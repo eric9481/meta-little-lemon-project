d888d7a298b2946e90cd54b198b616e7a2c7e232
Eric <eric.smith.9481@gmail.com>
2024-11-02 19:27:07 -0600

    confirmed booking page

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -1,8 +1,29 @@
+import { Center, Heading, Text, Box, Button } from '@chakra-ui/react'
+import { Link as ReactRouterLink } from 'react-router-dom'
 import React from 'react'
 
 const ConfirmedBooking = () => {
   return (
-    <div>ConfirmedBooking</div>
+    <Center width="100%" padding='20vh'>
+      
+        <Box maxWidth='860px' textAlign='center'>
+          <Heading
+            as="h1"
+            fontSize="5.3rem"
+            
+            fontWeight="400"
+            lineHeight="10"
+            marginTop='1rem'
+          >
+            Table Reserved
+          </Heading>
+          <Heading as="p" fontWeight="400" fontSize="3.3rem">
+            See you soon!
+          </Heading>
+          <Button as={ReactRouterLink} to='/' bg='brand.200' marginTop='3rem' size='lg'>Return Home</Button>
+        </Box>
+      
+    </Center>
   )
 }
 
